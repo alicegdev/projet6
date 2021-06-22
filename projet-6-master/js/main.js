6bdd366341fe6200e3eb04c1998037d96d4671f6
Alice Goarnisson <alice.goarnisson@gmail.com>
2021-06-22 17:24:18 +0200

    js doc

--- projet-6-master/js/main.js
+++ projet-6-master/js/main.js
@@ -1,34 +1,26 @@
-
-
-let cellulesInaccessibles = [];
-
+/** création des instances de la classe "Arme" */
 let mitraillette = new Arme("mitraillette", "./img/mitraillette.png", 10);
 let pistolet = new Arme("pistolet", "./img/pistolet.png", 5);
 let lancegrenades = new Arme("lance-grenades", "./img/lance-grenades.png", 50);
 let fusil = new Arme("fusil", "./img/fusil2.png", 25);
 
-let tabArmes = [];
+/**  tableau qui regroupe les armes */
+let tabArmes = [mitraillette, pistolet, lancegrenades, fusil];
 
-tabArmes[0] = mitraillette;
-tabArmes[1] = pistolet;
-tabArmes[2] = lancegrenades;
-tabArmes[3] = fusil;
+/**  création des instances de la classe "Joueurs" */
+let joueurUn = new Joueur("Edea", "./img/edea.png");
+let joueurDeux = new Joueur("Grounch", "./img/grounch.png");
 
-let joueurUn = new Joueur("Edea", mitraillette, "100", "./img/edea.png");
-let joueurDeux = new Joueur("Grounch", mitraillette, "100", "./img/grounch.png");
+/** tableau qui regroupe les joueurs */
+let tabJoueurs = [joueurUn, joueurDeux];
 
-let tabJoueurs = [];
-tabJoueurs[0] = joueurUn;
-tabJoueurs[1] = joueurDeux;
-
+/** création de la carte qui comprend : nombre de lignes, nombre de colonnes, nombre de cellules Inaccessibles, tableau des armes, tableau des joueurs */
 let carteUne = new Carte(6, 4, 3, tabJoueurs, tabArmes);
 
+/** récupération du bouton HTML et transformation en variable JS */
 let btnGenerer = document.getElementById("btn_generer");
 
-btnGenerer.addEventListener("click", () =>{
+/**  gestionnaire d'événement : au clic, la carte se génère */
+btnGenerer.addEventListener("click", () => {
     carteUne.generer();
-    carteUne.genererCasesInaccessibles();
-    carteUne.genererCasesArmes();
-    carteUne.genererCasesJoueurs();
-
 });
